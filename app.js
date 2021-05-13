152bb61d408e2fdd453e00d0e321008cd8dc3f31
rohan-joby <rohanjobyak@gmail.com>
2021-05-14 01:39:12 +0530

    wip: add list item functionality

--- app.js
+++ app.js
@@ -9,9 +9,25 @@ app.set("view engine", "ejs");
 //to use body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let items = [];
+
 app.get("/", (req, res) => {
-  const day = "remember";
-  res.render("List.ejs", { dataReceive: day });
+  const currentDay = new Date();
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  const day = currentDay.toLocaleDateString("en-IN", options);
+
+  res.render("List.ejs", { dayInfo: day, listItems: items });
+});
+
+app.post("/", (req, res) => {
+  const item = req.body.itemInput;
+  items.push(item);
+  res.redirect("/");
 });
 
 app.listen(3000, () => console.log("Port started at port 3000!"));
